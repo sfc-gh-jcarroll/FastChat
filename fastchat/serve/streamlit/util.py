671b779821d9fca1818aba20add68e6b810132e1
Thiago Teixeira <thiago.teixeira@snowflake.com>
2024-05-08 09:09:42 -0700

    Move social links to util.py

--- fastchat/serve/streamlit/util.py
+++ fastchat/serve/streamlit/util.py
@@ -49,17 +49,17 @@ def page_setup(title, icon, wide_mode=False):
 
     # Add page navigation
     with st.sidebar:
-        st.title("Chatbot Arena")
+        st.title("LMSYS Chatbot Arena")
 
-        PROMOTION_TEXT = "&nbsp; &bull; &nbsp;".join([f"[{name}]({url})" for name, url in [
-            ("Blog", "https://lmsys.org/blog/2023-05-03-arena/"),
-            ("GitHub", "https://github.com/lm-sys/FastChat"),
-            ("Dataset", "https://github.com/lm-sys/FastChat/blob/main/docs/dataset_release.md"),
-            ("Twitter", "https://twitter.com/lmsysorg"),
-            ("Discord", "https://discord.gg/HSWAKCrnFx"),
-        ]])
-
-        st.caption(PROMOTION_TEXT)
+        st.caption("&nbsp; &bull; &nbsp;".join([
+            f"[{name}]({url})" for name, url in [
+                ("Blog", "https://lmsys.org/blog/2023-05-03-arena/"),
+                ("GitHub", "https://github.com/lm-sys/FastChat"),
+                ("Dataset", "https://github.com/lm-sys/FastChat/blob/main/docs/dataset_release.md"),
+                ("Twitter", "https://twitter.com/lmsysorg"),
+                ("Discord", "https://discord.gg/HSWAKCrnFx"),
+            ]
+        ]))
 
         st.write("")
 
@@ -72,3 +72,53 @@ def page_setup(title, icon, wide_mode=False):
 
         st.write("")
         st.write("")
+
+        sidebar_container = st.container()
+
+        # TOS expander
+
+        with st.popover("Terms of Service", use_container_width=True):
+            st.write("""
+            **Users are required to agree to the following terms before using the service:**
+
+            The service is a research preview. It only provides limited safety measures and may generate
+            offensive content. It must not be used for any illegal, harmful, violent, racist, or sexual
+            purposes. Please do not upload any private information. The service collects user dialogue
+            data, including both text and images, and reserves the right to distribute it under a
+            Creative Commons Attribution (CC-BY) or a similar license.
+            """)
+
+
+        # Sponsors expander
+
+        SPONSOR_LOGOS = [
+            "https://storage.googleapis.com/public-arena-asset/kaggle.png",
+            "https://storage.googleapis.com/public-arena-asset/mbzuai.jpeg",
+            "https://storage.googleapis.com/public-arena-asset/a16z.jpeg",
+            "https://storage.googleapis.com/public-arena-asset/together.png",
+            "https://storage.googleapis.com/public-arena-asset/anyscale.png",
+            "https://storage.googleapis.com/public-arena-asset/huggingface.png",
+        ]
+
+        with st.popover("Sponsors", use_container_width=True):
+            st.write("""
+                We thank [Kaggle](https://www.kaggle.com/), [MBZUAI](https://mbzuai.ac.ae/),
+                [a16z](https://www.a16z.com/), [Together AI](https://www.together.ai/),
+                [Anyscale](https://www.anyscale.com/), [HuggingFace](https://huggingface.co/) for their generous
+                [sponsorship](https://lmsys.org/donations/).
+            """)
+
+            st.write("") # Vertical spacing
+
+            NUM_COLS = 3
+            for i, logo in enumerate(SPONSOR_LOGOS):
+                col_index = i % NUM_COLS
+
+                if col_index == 0:
+                    cols = st.columns(NUM_COLS, gap="medium")
+                    st.write("") # Vertical spacing
+
+                with cols[col_index]:
+                    st.image(logo, use_column_width="auto")
+
+    return sidebar_container
