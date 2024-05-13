06b2bf8d7d80059f3655858099192f9fd16905b5
Joshua Carroll <joshua.carroll@snowflake.com>
2024-05-13 15:15:55 -0700

    Add remaining pages (#2)
    * Add about page
    
    * Add initial vision page
    
    * Add battle placeholder

--- fastchat/serve/streamlit/pages/about.py
+++ fastchat/serve/streamlit/pages/about.py
@@ -1,8 +1,43 @@
 import streamlit as st
 
-from common import page_setup
+from common import page_setup, SPONSOR_LOGOS
 
 page_setup(
     title="About Us",
     icon="ℹ️",
 )
+
+st.markdown("""
+Chatbot Arena is an open-source research project developed by members from [LMSYS](https://lmsys.org) and UC Berkeley [SkyLab](https://sky.cs.berkeley.edu/). Our mission is to build an open platform to evaluate LLMs by human preference in the real-world.
+We open-source our [FastChat](https://github.com/lm-sys/FastChat) project at GitHub and release chat and human feedback dataset. We invite everyone to join us!
+
+### Arena Core Team
+- [Lianmin Zheng](https://lmzheng.net/) (co-lead), [Wei-Lin Chiang](https://infwinston.github.io/) (co-lead), [Ying Sheng](https://sites.google.com/view/yingsheng/home), [Joseph E. Gonzalez](https://people.eecs.berkeley.edu/~jegonzal/), [Ion Stoica](http://people.eecs.berkeley.edu/~istoica/)
+
+### Past Members
+- [Siyuan Zhuang](https://scholar.google.com/citations?user=KSZmI5EAAAAJ), [Hao Zhang](https://cseweb.ucsd.edu/~haozhang/)
+
+### Learn more
+- Chatbot Arena [paper](https://arxiv.org/abs/2403.04132), [launch blog](https://lmsys.org/blog/2023-05-03-arena/), [dataset](https://github.com/lm-sys/FastChat/blob/main/docs/dataset_release.md), [policy](https://lmsys.org/blog/2024-03-01-policy/)
+- LMSYS-Chat-1M dataset [paper](https://arxiv.org/abs/2309.11998), LLM Judge [paper](https://arxiv.org/abs/2306.05685)
+
+### Contact Us
+- Follow our [X](https://x.com/lmsysorg), [Discord](https://discord.gg/HSWAKCrnFx) or email us at lmsys.org@gmail.com
+- File issues on [GitHub](https://github.com/lm-sys/FastChat)
+- Download our datasets and models on [HuggingFace](https://huggingface.co/lmsys)
+
+### Acknowledgment
+We thank [SkyPilot](https://github.com/skypilot-org/skypilot) and [Gradio](https://github.com/gradio-app/gradio) team for their system support.
+We also thank [UC Berkeley SkyLab](https://sky.cs.berkeley.edu/), [Kaggle](https://www.kaggle.com/), [MBZUAI](https://mbzuai.ac.ae/), [a16z](https://www.a16z.com/), [Together AI](https://www.together.ai/), [Hyperbolic](https://hyperbolic.xyz/), [Anyscale](https://www.anyscale.com/), [HuggingFace](https://huggingface.co/) for their generous sponsorship. Learn more about partnership [here](https://lmsys.org/donations/).
+            """)
+
+NUM_COLS = 4
+for i, logo in enumerate(SPONSOR_LOGOS):
+    col_index = i % NUM_COLS
+
+    if col_index == 0:
+        cols = st.columns(NUM_COLS, gap="medium")
+        st.write("") # Vertical spacing
+
+    with cols[col_index]:
+        st.image(logo, use_column_width="auto")
--- fastchat/serve/streamlit/pages/vision.py
+++ fastchat/serve/streamlit/pages/vision.py
@@ -1,8 +1,126 @@
 import streamlit as st
+from streamlit_feedback import streamlit_feedback
 
-from common import page_setup
+from messages_ui import ConversationUI
+from schemas import ConversationMessage
+from common import (
+    get_models,
+    get_parameters,
+    chat_response,
+    MODELS_HELP_STR,
+    page_setup
+)
 
-page_setup(
+sidebar_container = page_setup(
     title="Vision Direct Chat",
     icon="👀",
+    wide_mode=True,
 )
+
+
+DEFAULT_MESSAGE = "Hello there! Let's chat?"
+
+# Store conversation state in streamlit session
+if "vision_conversation" not in st.session_state:
+    st.session_state["vision_conversation"] = ConversationUI()
+    st.session_state["vision_conversation"].add_message(
+        ConversationMessage(role="assistant", content=DEFAULT_MESSAGE),
+        render=False
+    )
+conversation_ui: ConversationUI = st.session_state["vision_conversation"]
+
+
+# Sidebar
+
+with sidebar_container:
+
+    models = [
+        "llava-v1.6-vicuna-13b",
+        "llava-v1.6-vicuna-7b",
+    ]
+    selected_model_name = st.selectbox(
+        "Choose a model to chat with:", models,
+        help="[LLaVA](https://github.com/haotian-liu/LLaVA): an open large language and vision assistant")
+    
+    # Override model so it functions (TODO: Fix to use real models)
+    selected_model_name = "snowflake/snowflake-arctic-instruct"
+
+    temperature, top_p, max_new_tokens = get_parameters(st.container())
+
+
+files, chat = st.columns([0.3, 0.7])
+
+
+# Files area
+
+""
+
+with files:
+    st.warning("This is a mockup app, the model can't yet see the image")
+    img = st.file_uploader("Add an image:", type=["png", "jpeg", "jpg"])
+    if img:
+        st.image(img, use_column_width="auto")
+    
+    # TODO: Add examples
+
+# Chat area
+
+""
+
+# Render the chat
+conversation_ui.render_all(container=chat)
+user_input = st.chat_input("Enter your message here.") or st.session_state.pop("regenerate", None)    
+
+if user_input:
+    conversation_ui.add_message(
+        ConversationMessage(role="user", content=user_input),
+        container=chat
+    )
+
+    chat_response(
+        conversation_ui,
+        selected_model_name,
+        temperature,
+        top_p,
+        max_new_tokens,
+        container=chat,
+    )
+
+response_controls = chat.container()
+
+def clear_history():
+    conversation_ui.conversation.reset_messages()
+    conversation_ui.add_message(
+        ConversationMessage(role="assistant", content=DEFAULT_MESSAGE),
+        render=False
+    )
+
+def flag():
+    st.toast("Flagged conversation as inappropriate", icon=":material/flag:")
+
+def regenerate():
+    st.session_state.regenerate = conversation_ui.conversation.messages[-2].content
+    del conversation_ui.conversation.messages[-2:]
+
+if len(conversation_ui.conversation.messages) > 1:
+    # TODO: Big loading skeleton always briefly shows on the hosted app
+    cols = response_controls.columns(4)
+
+    cols[0].button("⚠️ &nbsp; Flag", use_container_width=True, on_click=flag)
+    cols[1].button("🔄&nbsp; Regenerate", use_container_width=True, on_click=regenerate)
+    cols[2].button(
+        "🗑&nbsp; Clear history",
+        use_container_width=True,
+        on_click=clear_history,
+    )
+
+    with cols[3]:
+        feedback = streamlit_feedback(
+            feedback_type="thumbs",
+            #optional_text_label="[Optional] Please provide an explanation",
+            align="center",
+            key=f"feedback_{len(conversation_ui.conversation.messages)}",
+        )
+
+        if feedback:
+            st.toast("Feedback submitted!", icon=":material/rate_review:")
